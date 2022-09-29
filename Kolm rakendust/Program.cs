a94572582d345aff0be886c36fa6a081822c5602
Nikita Rimitsen <nikita.rimitsen@outlook.com>
2022-09-29 22:20:16 +0300

    PictureView

--- Kolm rakendust/Program.cs
+++ Kolm rakendust/Program.cs
@@ -16,7 +16,7 @@ namespace Kolm_rakendust
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Pictureviewer());
         }
     }
 }
